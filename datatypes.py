31e555fe7cdfb1a522f8584497b7abff84c4465e
nmccool <nmccool@students.mchenry.edu>
2026-03-09 14:01:01 -0500

    test update

---
+++ datatypes.py
@@ -0,0 +1,24 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+character_name = "Fierce McMillan"  # String
+lives = 3               # Integer
+player_rating = 88.86               # Float
+is_bad_guy = False              # Boolean
+
+print(f"Character Name: {character_name}")
+print(f"Lives: {lives}")
+print(f"Player Rating: {player_rating}")
+print(f"Is A Bad Guy: {is_bad_guy}")
+
+
